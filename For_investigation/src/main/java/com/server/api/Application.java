1822f1705ff8d5e9099c3b13c36ad06134f79dec
Nikolai <sild225@yandex.ru>
2020-03-31 00:35:33 +0300

    Added autorization

--- For_investigation/src/main/java/com/server/api/Application.java
+++ For_investigation/src/main/java/com/server/api/Application.java
@@ -1,6 +1,8 @@
 package com.server.api;
 
-import com.sun.net.httpserver.HttpServer;
+import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.mapping;
+import static java.util.stream.Collectors.toList;
 
 import java.io.IOException;
 import java.io.OutputStream;
@@ -10,14 +12,16 @@ import java.net.URLDecoder;
 import java.util.*;
 import java.util.regex.Pattern;
 
-import static java.util.stream.Collectors.*;
+import com.sun.net.httpserver.BasicAuthenticator;
+import com.sun.net.httpserver.HttpContext;
+import com.sun.net.httpserver.HttpServer;
 
 class Application {
 
     public static void main(String[] args) throws IOException {
         int serverPort = 8000;
         HttpServer server = HttpServer.create(new InetSocketAddress(serverPort), 0);
-        server.createContext("/api/hello", (exchange -> {
+        HttpContext context =server.createContext("/api/hello", (exchange -> {
 
             if ("GET".equals(exchange.getRequestMethod())) {
                 Map<String, List<String>> params = splitQuery(exchange.getRequestURI().getRawQuery());
@@ -25,7 +29,7 @@ class Application {
                 List<String> DefaultParams = new ArrayList<>();
                 DefaultParams.add(noNameText);
                 String name = params.getOrDefault("name", DefaultParams).stream().findFirst().orElse(noNameText);
-                //String name = params.getOrDefault("name", List.of(noNameText)).stream().findFirst().orElse(noNameText);
+
                 String respText = String.format("Hello %s!", name);
                 exchange.sendResponseHeaders(200, respText.getBytes().length);
                 OutputStream output = exchange.getResponseBody();
@@ -36,6 +40,13 @@ class Application {
             }
             exchange.close();
         }));
+        context.setAuthenticator(new BasicAuthenticator("myrealm") {
+            @Override
+            public boolean checkCredentials(String user, String pwd) {
+                return user.equals("admin") && pwd.equals("admin");
+            }
+        });
+
         server.setExecutor(null); // creates a default executor
         server.start();
     }
